c8df37ac3a818e047739968aa9e963ded25c48fb
evancasey <evan.casey92@gmail.com>
2013-04-18 17:24:48 -0700

    db insertions now work

--- CustomStreamListener.py
+++ CustomStreamListener.py
@@ -1,63 +1,124 @@
-"""
-Captures tweets from the Twitter Streaming API using OAuth and stores them in a Postgres DB
-"""
+#===================================================
+# CSCI181: Social Network Analysis Final Project
+#
+# Captures tweets from the Twitter Streaming API 
+#===================================================
 
 import sys
 import tweepy
+from sqlalchemy import create_engine
+from sqlalchemy.sql import select
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (
+	Column,
+	Integer,
+	String,
+	Boolean,
+	DateTime,
+)
+import json
 
-#Initialize empty list to store each queryChunk
-queryArray = []
-
-#Initialize an empty list to store all the queries
-queryTerms = []
-for line in open("SP500.txt", "r").readlines():
-	queryTerms.append(line.strip())
-
-#Break up queryTerms into chunks of 100 queries
-i = 0
-queryChunk = []
-while i < len(queryTerms):
-	queryChunk.append(queryTerms[i])
-	i += 1
-	if i % 400 == 0:
-		queryArray.append(queryChunk)
-		queryChunk = []
-	if i == len(queryTerms) and queryChunk:
-		queryArray.append(queryChunk)
-
-print 'twitter_stream no threading'
-print queryArray[0]
-print queryArray[1]
-
-#Needed for Oauth verification on the Twitter API
+#Oauth verification for the Twitter Streaming API
 CONSUMER_KEY = 'bN7Daq0GmSNA8Tbd7RFMeA'
 CONSUMER_SECRET = 'T3QP1XIFYzaQpxzuyKgVjgn1HfYtS6Ftwr7cAlcf8G4'
-
 ACCESS_TOKEN = '330015261-kRM2MzI4dXVNROv7Tv6Ok5LxaBabBLlX0kofiVZY'
 ACCESS_TOKEN_SECRET = 'a9bKfBc6eG89SqW9FxIYfgzR2U2hqwDkHrGsWVU'
 
+#using tweepy's built in oath handling
 auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
 
+#set up our local DB to store the tweets
+db = create_engine('sqlite:///stream.db')
+Base = declarative_base(bind=db)
+Session = scoped_session(sessionmaker(db))
+
+db.echo = True
+
+#setting up our models
+
+#class to store individual tweet traits
+class Tweet(Base):
+	__tablename__ = 'tweet'
+
+	id = Column(Integer, primary_key=True)
+	status_text = Column(String)
+	user_name = Column(Integer)
+	status_is_retweeted = Column(Boolean)
+	status_retweet_count = Column(Integer)
+	status_created_at = Column(DateTime, index = True)
+	status_source = Column(String)
+
+#class to store individual user traits
+class User(Base):
+	__tablename__ = 'user'
+
+	id = Column(Integer, primary_key=True)
+	user_name = Column(String)
+	user_time_zone = Column(String)
+
+	user_follow_request_sent = Column(Boolean)
+	user_statuses_count = Column(Integer)
+	user_followers_count = Column(Integer)
+	user_favourites_count = Column(Integer)
+	user_listed_count = Column(Integer)
+	user_mention_count = Column(Integer)
+	user_retweet_count = Column(Integer)
+
+#main streaming class
 class CustomStreamListener(tweepy.StreamListener):
 
 	def on_status(self, status):
 
-		text = status.text.encode('utf-8') if status.text else "null"
-		retweeted = status.retweeted
-		retweet_count = status.retweet_count
-		name = status.author.screen_name.encode('utf-8') if status.author.screen_name else "null"
-		followers_count = status.author.favourites_count
-		favourites_count = status.author.favourites_count
-		author_created_at = status.author.created_at
-		statuses_count = status.author.statuses_count
-		time_zone = status.author.time_zone.encode('utf-8') if status.author.time_zone else "null"
+		print "Storing Tweet...."
+		
+		t_store = Tweet(id = status.id,
+						status_text = status.text,
+						user_name = status.author.screen_name,
+						status_is_retweeted = status.retweeted,
+						status_retweet_count = status.retweet_count,
+						status_created_at = status.created_at,
+						status_source = status.source)
+
+		Session.add(t_store)
+
+		
+		status_id = status.id
+		user_id = status.author.id
+
+		user_name = status.author.screen_name
+
+		status_text = status.text.encode('utf-8') if status.text else "null"
+		status_is_retweeted = status.retweeted
+		status_retweet_count = status.retweet_count
+		user_listed_count = status.author.listed_count
+		user_follow_request_sent = status.author.follow_request_sent
+		
+		user_followers_count = status.author.favourites_count
+		user_favourites_count = status.author.favourites_count
+		user_statuses_count = status.author.statuses_count
+		user_time_zone = status.author.time_zone.encode('utf-8') if status.author.time_zone else "null"
 		status_created_at = status.created_at
-		source = status.source.encode('utf-8') if status.source.encode else "null"
+		status_source = status.source.encode('utf-8') if status.source.encode else "null"
 
 		try:
-		 	 print "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (text, retweeted, retweet_count, name, followers_count, 
-		 	 	favourites_count, author_created_at, statuses_count, time_zone, status_created_at, source)
+		 	 print status_text
+
+		 	 print "Committing.."
+
+		 	 Session.commit()
+
+		 	 print "Printing out the rows"
+		 	 
+		 	 s = select([Tweet])
+			 result = s.execute()
+			 
+			 for row in result:
+			    print row
+
+
+
 
 
 		except Exception, e:
@@ -74,7 +135,18 @@ class CustomStreamListener(tweepy.StreamListener):
 		print >> sys.stderr, 'Timeout...'
 		return True #Don't kill the stream
 
-listener=CustomStreamListener()
+if __name__ == '__main__':
+	#using twitter handles as keywords (must be <400)
+	queryUsers = []
+	for line in open("users.txt", "r").readlines():
+		queryUsers.append(line.strip())
 
-stream=tweepy.streaming.Stream(auth,listener)
-stream.filter(track=queryArray[0])
+	#using tickers as keywords (must be <400)
+	queryTerms = []
+	for line in open("tickers.txt", "r").readlines():
+		queryTerms.append(line.strip())
+
+	#call our main streaming handler
+	listener=CustomStreamListener()
+	stream=tweepy.streaming.Stream(auth,listener)
+	stream.filter(track=queryUsers)
